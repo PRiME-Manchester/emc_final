840976f17523a4a286d33edefed4f287b6a73549
Patrick Camilleri <patrick.camilleri@manchester.ac.uk>
2016-03-11 13:56:23 +0100

    experimenting with doxygen

---
+++ doxygen/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['fault_5ft',['fault_t',['../structfault__t.html',1,'']]]
+];
---
+++ doxygen/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['info_5frx_5ft',['info_rx_t',['../structinfo__rx__t.html',1,'']]],
+  ['info_5ftx_5ft',['info_tx_t',['../structinfo__tx__t.html',1,'']]]
+];
